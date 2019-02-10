c2d651d3a633447d1d4babb1fb83a3d67462731b
meenakshi55 <47452017+meenakshi55@users.noreply.github.com>
2019-02-10 10:39:15 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -3,13 +3,13 @@ int main()
 {
   char v;
   scanf("%c",&v);
-  if(v=='a'||v=='b'||v=='c'||v=='d'||v=='e'||v=='f'||v=='g'||v=='h'||v=='i'||v=='j'||v=='k'||v=='l'||v=='m'||v=='n'||v=='o'||v=='p'||v=='q'||v=='r'||v=='s'||v=='t'||v=='u'||v=='v'||v=='w'||v=='x'||v=='y'||v=='z')
-  {
-    printf("Alphabet");
-  }
-  else if(v=='?'||v=='@'||v=='#'||v=='$')
+ if(v=='?'||v=='@'||v=='#'||v=='$')
   {
     printf("no");
+  }
+  else
+  {
+    printf("Alphabet");
   }
   return 0;
 }
